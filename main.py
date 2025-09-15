c0d60708b0b11bd01cc647bae67485d7fd67e987
AdnanSaif322 <adnan.saif.322@gmail.com>
2025-09-15 22:12:19 +0600

    read the book from terminal

---
+++ main.py
@@ -0,0 +1,11 @@
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+main()
